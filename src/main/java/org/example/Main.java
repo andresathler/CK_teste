348d3f68ab63ce24c53b3517bd1df40e87da86aa
André Avelar Moriya Sathler <andremoriya.sathler@gmail.com>
2024-03-07 19:19:08 -0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //print
+        System.out.println("Engenharia Econômica para Software");
+
+    }
+}
